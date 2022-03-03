3b88d4b30550c9cfe493123d8f9ee68a7c46dbc1
SrZurkon <al405806@uji.es>
2022-03-03 11:40:11 +0100

    test

--- src/test/java/p1/RowWithLabelTest.java
+++ src/test/java/p1/RowWithLabelTest.java
@@ -1,6 +1,7 @@
 package p1;
-
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +9,12 @@ class RowWithLabelTest {
 
     @Test
     void getLabel() {
+        List<Double> l = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            l.add(Math.random() % 10);
+        }
+        String label = "I am a testing label";
+        RowWithLabel r = new RowWithLabel(l, label);
+        assertEquals(r.label, r.getLabel());
     }
 }
